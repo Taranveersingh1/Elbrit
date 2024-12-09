616fe4e1cc40c2dcebde10bc7c533bcaf0d3acb1
Taran <taranveersingh130@gmail.com>
2024-12-09 22:22:57 +0530

    tarann

--- src/app/BetterIngredients.js
+++ src/app/BetterIngredients.js
@@ -41,7 +41,6 @@ const BetterIngredients = () => {
           </p>
         </div>
 
-        {/* Ingredient Cards */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {ingredients.map((ingredient, index) => (
             <div
